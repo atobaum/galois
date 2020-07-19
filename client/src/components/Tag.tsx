70ab9d1e1f125a79f2c4786371f34561bf77ac7d
Atobaum <atobaum@gmail.com>
2020-07-19 23:26:31 +0900

    New component: ZettelEditor

---
+++ client/src/components/Tag.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+/** @jsx jsx */
+import { jsx, css } from "@emotion/core";
+
+const TagCss = css`
+  color: gray;
+`;
+
+type TagProps = {
+  onClick?: () => void;
+  children: string;
+};
+
+function Tag({ onClick, children }: TagProps) {
+  return (
+    <span css={TagCss} onClick={onClick ? onClick : () => {}}>
+      #{children}
+    </span>
+  );
+}
+
+export default Tag;
---
+++ client/src/components/ZettelEditor.tsx
@@ -0,0 +1,114 @@
+import React, { useState, useRef } from "react";
+
+/** @jsx jsx */
+import { jsx, css } from "@emotion/core";
+import Tag from "./Tag";
+
+const ZettelEditorCss = css`
+  display: flex;
+  max-width: 600px;
+  flex-direction: column;
+  border: 1px solid gray;
+  border-radius: 15px;
+  padding: 1rem;
+  textarea {
+    resize: vertical;
+  }
+  * {
+    font-size: 1.5rem;
+  }
+`;
+
+type NoteEditorProps = {
+  onSubmit: (args: any) => void;
+};
+
+function ZettelEditor({ onSubmit }: NoteEditorProps) {
+  const [content, setContent] = useState("");
+  const [title, setTitle] = useState("");
+  const [tags, setTags] = useState<string[]>([]);
+  const [tagInput, setTagInput] = useState("");
+  const [source, setSource] = useState("");
+  const contentRef = useRef<HTMLTextAreaElement>(null);
+
+  const handleTagOnKeypress: React.KeyboardEventHandler<HTMLInputElement> = (
+    evt
+  ) => {
+    if (evt.key === "Enter" || evt.which === 13) {
+      evt.preventDefault();
+      const tag = tagInput.trim();
+      if (tag.length === 0) return;
+      setTags([...tags, tag]);
+      setTagInput("");
+    }
+  };
+
+  return (
+    <form
+      onSubmit={(evt) => {
+        evt.preventDefault();
+        if (content.trim().length === 0) return;
+        onSubmit({
+          content: content.trim(),
+          title: title.trim(),
+          tags,
+          source: source.trim(),
+        });
+        setContent("");
+        setTitle("");
+        setSource("");
+        contentRef.current!.focus();
+      }}
+      css={ZettelEditorCss}
+    >
+      <textarea
+        value={content}
+        onChange={(e) => setContent(e.target.value)}
+        placeholder="Type content..."
+        ref={contentRef as any}
+        rows={4}
+      />
+      <input
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        placeholder="Type title..."
+      />
+      <div
+        css={css`
+          display: flex;
+          span {
+            padding: 0 15px;
+          }
+          input {
+            flex-grow: 1;
+          }
+        `}
+      >
+        {tags.map((tag) => (
+          <Tag
+            key={tag}
+            onClick={() => {
+              setTags(tags.filter((t) => t !== tag));
+            }}
+          >
+            {tag}
+          </Tag>
+        ))}
+        <input
+          value={tagInput}
+          onChange={(e) => setTagInput(e.target.value)}
+          onKeyPress={handleTagOnKeypress}
+          placeholder="Type tag..."
+        />
+      </div>
+      <input
+        value={source}
+        onChange={(e) => setSource(e.target.value)}
+        placeholder="Type source..."
+      />
+      <button>Submit</button>
+    </form>
+  );
+}
+
+export default ZettelEditor;
