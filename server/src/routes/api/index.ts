7cec69f9e79452560b868ea076a63b90f754c890
atobaum <atobaum@gmail.com>
2020-08-31 23:24:07 +0900

    feat: Create /api/zettels
    Send mocked zettel data

--- server/src/routes/api/index.ts
+++ server/src/routes/api/index.ts
@@ -3,8 +3,44 @@ import auth from "./auth";
 
 const api = new Router();
 api.use("/auth", auth.routes());
-api.get("/todo", (ctx) => {
-  ctx.body = [1, 2, 3];
+api.get("/zettel", async (ctx) => {
+  ctx.body = "send";
+});
+api.delete("/zettel", async (ctx) => {
+  ctx.body = "send";
+});
+api.put("/zettel", async (ctx) => {
+  ctx.body = "send";
+});
+api.get("/zettels", async (ctx) => {
+  ctx.body = {
+    zettels: [
+      {
+        id: 1,
+        content: "test11",
+        tags: ["inbox", "tag1"],
+        title: null,
+      },
+      {
+        id: 2,
+        content: "test2",
+        tags: ["tag2"],
+        title: null,
+      },
+      {
+        id: 3,
+        content: "test3",
+        tags: ["tag3"],
+        title: null,
+      },
+      {
+        id: 4,
+        content: "test4",
+        tags: ["tag4", "inbox"],
+        title: "title",
+      },
+    ],
+  };
 });
 
 export default api;
