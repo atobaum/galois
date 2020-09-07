64897788634848a56a24b1348963053f0746300d
atobaum <atobaum@gmail.com>
2020-09-07 20:59:51 +0900

    feat: Create creating zettel api call function

--- client/src/api/zettelApi.ts
+++ client/src/api/zettelApi.ts
@@ -6,3 +6,15 @@ export const getZettels = async (): Promise<Zettel[]> => {
   if (data.status === 200) return data.data.zettels;
   else throw new Error("/api/zettels error");
 };
+
+export const createZettel = async (
+  createZettelDTO: Pick<Zettel, "title" | "content" | "tags">
+): Promise<Zettel> => {
+  const data = await axios.post("/api/zettel", createZettelDTO);
+  if (data.status === 200) {
+    return data.data;
+  } else {
+    // TODO: error handling
+    throw new Error("createZettel error");
+  }
+};
