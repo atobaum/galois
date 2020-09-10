b0d4ebacb72c7889d4f4867a79ce989f3e627cd0
atobaum <atobaum@gmail.com>
2020-09-11 02:58:45 +0900

    fix: Re-enable jwt middleware

--- server/src/loaders/koaLoader.ts
+++ server/src/loaders/koaLoader.ts
@@ -2,12 +2,12 @@ import Koa from "koa";
 import logger from "koa-logger";
 import bodyParser from "koa-bodyparser";
 import router from "../routes";
-// import jwtMiddleware from "./middleware/jwtMiddleware";
+import jwtMiddleware from "../middleware/jwtMiddleware";
 
 export default async ({ app }: { app: Koa }): Promise<Koa> => {
   app.use(logger());
   app.use(bodyParser());
-  // app.use(jwtMiddleware);
+  app.use(jwtMiddleware);
   app.use(router.routes()).use(router.allowedMethods());
 
   return app;
