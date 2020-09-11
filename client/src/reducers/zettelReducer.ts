e46a82556975b095487072038c9169043d73c693
atobaum <atobaum@gmail.com>
2020-09-11 23:27:01 +0900

    feat(redux): Create deleting zettel action and its handler
    action: "zettel/DELETE_ZETTEL"

--- client/src/reducers/zettelReducer.ts
+++ client/src/reducers/zettelReducer.ts
@@ -1,13 +1,21 @@
 import { Zettel } from "../models/Zettel";
 
 const CREATE_ZETTEL = "zettel/CREATE_ZETTEL" as const;
+const DELETE_ZETTEL = "zettel/DELETE_ZETTEL" as const;
 
 export const addZetel = (zettel: Zettel) => ({
   type: CREATE_ZETTEL,
   payload: zettel,
 });
 
-type InboxAction = ReturnType<typeof addZetel>;
+export const deleteZettelAction = (id: number) => ({
+  type: DELETE_ZETTEL,
+  payload: id,
+});
+
+type InboxAction =
+  | ReturnType<typeof addZetel>
+  | ReturnType<typeof deleteZettelAction>;
 
 type InboxState = {
   zettels: Zettel[];
@@ -24,6 +32,9 @@ export default function inboxReducer(
   switch (action.type) {
     case CREATE_ZETTEL:
       return { zettels: [...state.zettels, action.payload] };
+    case DELETE_ZETTEL:
+      return { zettels: state.zettels.filter((z) => z.id !== action.payload) };
+
     default:
       return state;
   }
