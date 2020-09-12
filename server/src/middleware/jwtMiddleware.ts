67e78a97f14c5f6bd87f5c47c40c87a2e829bfd1
atobaum <atobaum@gmail.com>
2020-09-13 00:10:08 +0900

    fix: Comment refresh token logic

--- server/src/middleware/jwtMiddleware.ts
+++ server/src/middleware/jwtMiddleware.ts
@@ -16,7 +16,7 @@ export default async function jwtMiddleware(
   const { authorization } = ctx.request.header;
   if (!accessToken && authorization) accessToken = authorization.split(" ")[1];
 
-  const refreshToken = ctx.cookies.get("refresh_token");
+  // const refreshToken = ctx.cookies.get("refresh_token");
 
   if (accessToken) {
     try {
@@ -36,49 +36,49 @@ export default async function jwtMiddleware(
     }
   }
 
-  if (!accessToken && refreshToken) {
-    try {
-      const refreshTokenData = await decodeToken<RefreshTokenData>(
-        refreshToken
-      );
-      if (refreshTokenData.sub !== "refresh_token") {
-        ctx.status = 401;
-        ctx.body = { error: "Invalid refresh token" };
-        return;
-      }
+  // if (!accessToken && refreshToken) {
+  //   try {
+  //     const refreshTokenData = await decodeToken<RefreshTokenData>(
+  //       refreshToken
+  //     );
+  //     if (refreshTokenData.sub !== "refresh_token") {
+  //       ctx.status = 401;
+  //       ctx.body = { error: "Invalid refresh token" };
+  //       return;
+  //     }
 
-      //validity check in db
-      const refreshTokenDb = await getRepository(RefreshToken).findOne({
-        where: { id: refreshTokenData.token_id },
-        relations: ["user"],
-      });
+  //     //validity check in db
+  //     const refreshTokenDb = await getRepository(RefreshToken).findOne({
+  //       where: { id: refreshTokenData.token_id },
+  //       relations: ["user"],
+  //     });
 
-      if (refreshTokenDb === undefined) {
-        ctx.status = 500;
-        ctx.body = { error: "Inconsistand DB: refresh token" };
-        return;
-      }
+  //     if (refreshTokenDb === undefined) {
+  //       ctx.status = 500;
+  //       ctx.body = { error: "Inconsistand DB: refresh token" };
+  //       return;
+  //     }
 
-      if (refreshTokenDb.disabled) {
-        ctx.status = 401;
-        ctx.body = { error: "Disabled refresh token" };
-        return;
-      }
+  //     if (refreshTokenDb.disabled) {
+  //       ctx.status = 401;
+  //       ctx.body = { error: "Disabled refresh token" };
+  //       return;
+  //     }
 
-      const diff = refreshTokenData.exp * 1000 - new Date().getTime();
-      let tokens = null;
-      if (diff < 2 * 24 * 60 * 60 * 1000) {
-        tokens = await refreshTokenDb.user.generateAuthTokens();
-      } else {
-        tokens = {
-          accessToken: await refreshTokenDb.user.generateAccessToken(),
-        };
-      }
-      setTokens(ctx, tokens);
-    } catch (e) {
-      // if (e.name !== "TokenExpiredError") // Logging
-      clearTokens(ctx);
-    }
-  }
+  //     const diff = refreshTokenData.exp * 1000 - new Date().getTime();
+  //     let tokens = null;
+  //     if (diff < 2 * 24 * 60 * 60 * 1000) {
+  //       tokens = await refreshTokenDb.user.generateAuthTokens();
+  //     } else {
+  //       tokens = {
+  //         accessToken: await refreshTokenDb.user.generateAccessToken(),
+  //       };
+  //     }
+  //     setTokens(ctx, tokens);
+  //   } catch (e) {
+  //     // if (e.name !== "TokenExpiredError") // Logging
+  //     clearTokens(ctx);
+  //   }
+  // }
   return next();
 }
