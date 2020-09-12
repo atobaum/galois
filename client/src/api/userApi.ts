7bc4d9f663c86ed33cae100ac1ca05c05fc5027b
atobaum <atobaum@gmail.com>
2020-09-13 01:20:35 +0900

    feat: Fetch current user when initializing app

--- client/src/api/userApi.ts
+++ client/src/api/userApi.ts
@@ -3,4 +3,5 @@ import axios from "axios";
 export const getCurrentUser = async () => {
   const data = await axios.get("/api/user/mine");
   if (data.status === 200) return data.data;
+  else if (data.status === 204) return null;
 };
