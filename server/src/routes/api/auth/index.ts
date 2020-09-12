29c6acfe8fec5f56c364a81c7aa1294e32093aff
atobaum <atobaum@gmail.com>
2020-09-13 00:47:38 +0900

    fix: Delete local login route
    - No more used

--- server/src/routes/api/auth/index.ts
+++ server/src/routes/api/auth/index.ts
@@ -1,44 +1,14 @@
 import Router from "koa-router";
-import { getRepository } from "typeorm";
-import User from "../../../entity/User";
-import { setTokens, clearTokens } from "../../../lib/token";
+import { clearTokens } from "../../../lib/token";
 import googleRouter from "./google";
 
 const auth = new Router();
 
 auth.use("/google", googleRouter.routes());
 
-auth.post("/login", async (ctx) => {
-  const { email, password } = ctx.request.body;
-  if (!(email && password)) {
-    ctx.body = {
-      error: "Missing email or password",
-    };
-    ctx.status = 401;
-    return;
-  }
-
-  const user = await getRepository(User).findOne({ email });
-  if (user && (await user.checkPassword(password))) {
-    const userToken = await user.generateAuthTokens();
-    setTokens(ctx, userToken);
-
-    ctx.body = {
-      access_token: userToken.accessToken,
-      refresh_token: userToken.refreshToken,
-    };
-  } else {
-    ctx.body = {
-      error: "Invalid user infomation",
-    };
-    ctx.status = 401;
-    return;
-  }
-});
-
 auth.post("/logout", async (ctx) => {
   clearTokens(ctx);
-  ctx.status = 205;
+  ctx.redirect("/");
 });
 
 export default auth;
