b1fb291d6bf19e7ac4428ac46a5e60baf72fec38
atobaum <atobaum@gmail.com>
2020-09-14 23:44:45 +0900

    fix: Change postgresqlLoader to real loader #22

--- server/src/loaders/postgresqlLoader.ts
+++ server/src/loaders/postgresqlLoader.ts
@@ -2,27 +2,33 @@ import { ConnectionOptions, createConnection, Connection } from "typeorm";
 import entities from "../entity";
 import config from "../config";
 
-const postgrasqlLoader: Promise<Connection> = new Promise((resolve, reject) => {
-  const connectionOptions: ConnectionOptions = {
-    type: "postgres",
-    host: config.postgresql.host,
-    database: config.postgresql.database,
-    username: config.postgresql.username,
-    password: config.postgresql.password,
-    port: config.postgresql.port,
-    synchronize: process.env.TYPEORM_SYNCHRONIZE === "true",
-    logging: process.env.TYPEORM_LOGGING === "true",
-    entities,
-  };
-  createConnection(connectionOptions)
-    .then((conn) => {
-      console.log("Database connected.");
-      resolve(conn);
-    })
-    .catch((e) => {
-      console.log("Cannot connect database:");
-      reject(e);
-    });
-});
-
-export default postgrasqlLoader;
+export default function postgrasqlLoader(): Promise<Connection> {
+  return new Promise((resolve, reject) => {
+    const connectionOptions: ConnectionOptions = {
+      type: "postgres",
+      host: config.postgresql.host,
+      port: config.postgresql.port,
+      database: config.postgresql.database,
+      username: config.postgresql.username,
+      password: config.postgresql.password,
+      logging: process.env.TYPEORM_LOGGING === "true",
+      entities,
+      // heroku 연결 하기 위해
+      ssl: true,
+      extra: {
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      },
+    };
+    createConnection(connectionOptions)
+      .then((conn) => {
+        console.log("Database connected.");
+        resolve(conn);
+      })
+      .catch((e) => {
+        console.log("Cannot connect database:");
+        reject(e);
+      });
+  });
+}
