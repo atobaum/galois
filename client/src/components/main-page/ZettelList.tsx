3fef064353df0d50efe510ceb31ff0b5581f4f4d
Atobaum <kuuften@gmail.com>
2020-09-26 00:28:27 +0900

    feat: ZettelList 수정
    - 필요없는 기능 삭제(최소로 줄임)
    - 클릭했을 때 에디터에 setting(redux)

--- client/src/components/main-page/ZettelList.tsx
+++ client/src/components/main-page/ZettelList.tsx
@@ -1,42 +1,33 @@
 // eslint-disable-next-line
 import React from "react";
-import ZettelCard from "../ZettelCard";
 
 /** @jsx jsx */
 import { jsx, css } from "@emotion/core";
 import { useSelector, useDispatch } from "react-redux";
 import { RootState } from "../../reducers";
-import { deleteZettel } from "../../api/zettelApi";
-import { deleteZettelAction } from "../../reducers/zettelReducer";
 import ZettelListItem from "./ZettelListItem";
+import { setEditor } from "../../reducers/editorReducer";
 
 const ZettelListCss = css`
   display: flex;
   flex-direction: column;
 `;
 
-type ZettelListProps = {
-  filter?: {
-    tag?: string;
-  };
-};
+type ZettelListProps = {};
+
 function ZettelList(props: ZettelListProps) {
-  const filter = props.filter || {};
   const zettels = useSelector((state: RootState) => state.zettel.zettels);
   const dispatch = useDispatch();
-  const onDeleteZettel = async (id: number) => {
-    const deleted = await deleteZettel(id);
-    if (deleted) dispatch(deleteZettelAction(id));
-  };
   return (
     <div css={ZettelListCss} className="zettel-list">
-      {zettels
-        .filter((zettel) => {
-          return filter.tag ? zettel.tags.includes(filter.tag!) : true;
-        })
-        .map((note) => (
-          <ZettelListItem key={note.id} {...note} onDelete={onDeleteZettel} />
-        ))}
+      {zettels.map((note) => (
+        <ZettelListItem
+          key={note.id}
+          {...note}
+          onTagClick={() => {}}
+          onClick={() => dispatch(setEditor(note))}
+        />
+      ))}
     </div>
   );
 }
---
+++ client/src/components/main-page/__test__/ZettelList.test.tsx
@@ -0,0 +1,22 @@
+import { fireEvent, render } from "@testing-library/react";
+import React from "react";
+import { useSelector } from "react-redux";
+import ZettelList from "../ZettelList";
+
+import { initZettels } from "../../../__mocks__/react-redux";
+
+jest.mock("react-redux");
+
+describe("ZettelList", () => {
+  it("Zettel을 클릭했을 때 redux dispatch", () => {
+    const { container } = render(<ZettelList />);
+
+    const firstZettel = container.firstChild?.firstChild;
+    fireEvent.click(firstZettel!);
+
+    const editorState = useSelector((state: any) => state.editor);
+    expect(editorState.isEditing).toBe(true);
+    expect(editorState.isNew).toBe(false);
+    expect(editorState.zettel.id).toBe(initZettels[0].id);
+  });
+});
