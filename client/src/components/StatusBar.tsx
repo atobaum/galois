b15d149c58a1d6322a9da77b7ca54b40417ae688
Atobaum <kuuften@gmail.com>
2020-09-27 01:01:23 +0900

    feat: Add "new" button in status bar

--- client/src/components/StatusBar.tsx
+++ client/src/components/StatusBar.tsx
@@ -1,16 +1,26 @@
 import React from "react";
 /** @jsx jsx */
 import { jsx, css } from "@emotion/core";
-import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import useCurrentUser from "../hooks/useCurrentUser";
+import { useDispatch } from "react-redux";
+import { startEdit } from "../reducers/editorReducer";
 
 const StatusBarCss = css``;
 
 const StatusBar: React.FC = () => {
   const user = useCurrentUser();
+  const disptach = useDispatch();
+
   return (
     <div className="status-bar" css={StatusBarCss}>
+      <button
+        onClick={() => {
+          disptach(startEdit(null));
+        }}
+      >
+        New
+      </button>
       {user && (
         <span>
           {user.picture && (
