41116c0b86bdcb567c7d01cd0848f6a428c1e7f7
Atobaum <kuuften@gmail.com>
2020-09-29 01:08:23 +0900

    feat: Create MemoryUserRepository for test

---
+++ server/src/domain/user/MemoryUserRepository.ts
@@ -0,0 +1,68 @@
+import SocialAccount from "./entity/SocialAccount";
+import User from "./entity/User";
+import IUserRepository from "./IUserRepository";
+
+const initUsers: { [key: number]: User } = {
+  1: User.create(
+    {
+      email: "test1@test.com",
+      thumbnail: null,
+      username: "test1",
+      socialAccounts: [new SocialAccount("google", "123")],
+    },
+    1
+  ),
+  2: User.create(
+    {
+      email: "test2@test.com",
+      thumbnail: "thumb",
+      username: "test2",
+      socialAccounts: [
+        new SocialAccount("facebook", "125"),
+        new SocialAccount("google", "127"),
+      ],
+    },
+    2
+  ),
+};
+
+export default class MemoryUserRepository implements IUserRepository {
+  db: { [k: number]: User };
+  nextId: number;
+  constructor() {
+    this.db = initUsers;
+    this.nextId = 3;
+  }
+
+  async findBySocialAccount(
+    provider: string,
+    socialId: string
+  ): Promise<User | null> {
+    let user = null;
+    for (let id in this.db) {
+      if (
+        this.db[id].socialAccounts?.some(
+          (sa) => sa.provider === provider && sa.socialId === socialId
+        )
+      )
+        user = this.db[id];
+    }
+    return user;
+  }
+
+  async findById(id: number, option?: any): Promise<User | null> {
+    const user = this.db[id] || null;
+    return user;
+  }
+  async save(entity: User): Promise<number> {
+    if (!entity.id) {
+      entity.id = this.nextId;
+      this.nextId++;
+    }
+    this.db[entity.id] = entity;
+    return entity.id;
+  }
+  async delete(id: number): Promise<void> {
+    delete this.db[id];
+  }
+}
---
+++ server/src/domain/user/entity/__test__/MemoryUserRepository.test.ts
@@ -0,0 +1,33 @@
+import { clearScreenDown } from "readline";
+import MemoryUserRepository from "../../MemoryUserRepository";
+import User from "../User";
+
+describe("MemoryUserRepository", () => {
+  const repo = new MemoryUserRepository();
+  it("findById", async (done) => {
+    const user = await repo.findById(1);
+    expect(user.id).toBe(1);
+    done();
+  });
+
+  it("findBySocialAccount", async (done) => {
+    const user = await repo.findBySocialAccount("google", "127");
+    expect(user.id).toBe(2);
+    done();
+  });
+
+  it("save", async (done) => {
+    const user = User.create({
+      email: "asdf",
+      thumbnail: null,
+      username: "adfs",
+    });
+    const id = await repo.save(user);
+    const getUser = await repo.findById(id);
+
+    expect(id).toBeTruthy();
+    expect(user.id).toBe(id);
+    expect(getUser).toBe(user);
+    done();
+  });
+});
