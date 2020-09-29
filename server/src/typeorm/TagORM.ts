2dd52e2e345d9b873838e9bacdd472ba17a82b11
Atobaum <kuuften@gmail.com>
2020-09-30 04:33:27 +0900

    feat: Crate TagORM.findOrCreate

--- server/src/typeorm/TagORM.ts
+++ server/src/typeorm/TagORM.ts
@@ -3,6 +3,7 @@ import {
   Entity,
   Column,
   CreateDateColumn,
+  getRepository,
 } from "typeorm";
 
 @Entity({ name: "tag" })
@@ -15,4 +16,18 @@ export default class TagORM {
 
   @CreateDateColumn({ type: "timestamptz", name: "created_at" })
   readonly createdAt!: Date;
+
+  static async findOrCreate(name: string): Promise<TagORM> {
+    const repo = getRepository(TagORM);
+    let tag = await repo.findOne({ where: { name } });
+    if (!tag) {
+      tag = new TagORM(name);
+      await repo.save(tag);
+    }
+
+    return tag;
+  }
+  constructor(name: string) {
+    this.name = name;
+  }
 }
