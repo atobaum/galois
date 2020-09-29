4571b7fe059c096c69bc10152fec859c2e844b93
Atobaum <kuuften@gmail.com>
2020-09-30 04:35:12 +0900

    feat: Create skeleton of Zettel

--- server/src/domain/zettel/entity/Zettle.ts
+++ server/src/domain/zettel/entity/Zettle.ts
@@ -1,12 +1,33 @@
+import { ZettelDTO } from "../../../graphql/zettelSchema";
+import Entity from "../../shared/Entity";
 import Revision from "./Revision";
 
-export default class Zettel {
-  id?: number;
+export default class Zettel extends Entity {
   version?: number;
   uuid?: string;
-  title?: string;
-  content?: string;
-  userId?: number;
-  createdAt?: Date;
-  revisions?: Revision[];
+  title: string | null;
+  userId: number;
+  createdAt: Date;
+  revision: Revision;
+  tags: string[];
+
+  constructor(args: {
+    title: string | null;
+    userId: number;
+    createdAt: Date;
+    revision: Revision;
+    tags: string[];
+    id: number | null;
+  }) {
+    super(args.id);
+    this.title = args.title;
+    this.userId = args.userId;
+    this.createdAt = args.createdAt;
+    this.revision = args.revision;
+    this.tags = args.tags;
+  }
+
+  public toDTO(): ZettelDTO {
+    throw new Error();
+  }
 }
