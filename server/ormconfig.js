db69b149c20038568745c8af70c028daff7a978b
Atobaum <kuuften@gmail.com>
2020-09-29 14:21:19 +0900

    ci: Github action에서 postgres 접속 못하는 문제 해결
    ssl 지원을 안한다고 해서 github action일때는 ssl disable

--- server/ormconfig.js
+++ server/ormconfig.js
@@ -6,22 +6,28 @@ dotenv.config({
 
 const env = process.env;
 
-module.exports = {
+const config = {
   type: "postgres",
   host: env.TYPEORM_HOST,
   port: Number(env.TYPEORM_PORT),
   username: env.TYPEORM_USERNAME,
   password: env.TYPEORM_PASSWORD,
   database: env.TYPEORM_DATABASE,
-  ssl: true,
-  extra: {
-    ssl: {
-      rejectUnauthorized: false,
-    },
-  },
   synchronize: true,
   logging: false,
   entities: ["src/entity/**/*.ts"],
   migrations: ["src/migration/**/*.ts"],
   subscribers: ["src/subscriber/**/*.ts"],
 };
+
+// Heroku postgresql 접속 안되서
+if (!env.GITHUB_RUN_ID) {
+  config.ssl = true;
+  config.extra = {
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  };
+}
+
+module.exports = config;
