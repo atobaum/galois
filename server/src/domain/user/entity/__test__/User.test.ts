7190b0e105420915cf08d0d24513ba6f9ba043e4
Atobaum <kuuften@gmail.com>
2020-10-01 05:19:48 +0900

    feat: Not saved user cannot generate access token

---
+++ server/src/domain/user/entity/__test__/User.test.ts
@@ -0,0 +1,35 @@
+import { doesNotThrow } from "assert";
+import jwt from "jsonwebtoken";
+import User from "../User";
+
+describe("User", () => {
+  const userData = {
+    username: "testuser",
+    email: "email@asf.com",
+    thumbnail: "asf;lk",
+  };
+  // TODO email validation
+
+  // it("setter/getter", () => {});
+
+  // it("add social account", () => {});
+
+  it("cannot generate access token of new user", () => {
+    const user = User.create(userData);
+    return expect(user.generateAccessToken()).rejects.toThrow();
+  });
+
+  it("generate accesstoken", async () => {
+    const user = User.create({ ...userData }, 2);
+    const token = await user.generateAccessToken();
+    const tokenData = jwt.decode(token) as any;
+
+    expect(tokenData).toBeTruthy();
+    expect(tokenData.id).toBe(2);
+  });
+
+  it("getDTO", () => {
+    const user = User.create(userData);
+    expect(user.getDTO()).toEqual(userData);
+  });
+});
