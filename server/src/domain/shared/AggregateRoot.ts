ea7054a100cf1aa9d2645782cae3100a67c72f56
Atobaum <kuuften@gmail.com>
2020-10-01 07:47:41 +0900

    feat: Change aggregate root change type

--- server/src/domain/shared/AggregateRoot.ts
+++ server/src/domain/shared/AggregateRoot.ts
@@ -1,18 +1,18 @@
 import Entity from "./Entity";
 
-type Change = {
-  type: string;
-  payload: any;
-};
+type Change = [string, any?];
 
-export default abstract class AggregateRoot<K = number> extends Entity<K> {
-  protected changes: Change[] = [];
+export default abstract class AggregateRoot<
+  C extends Change,
+  K = number
+> extends Entity<K> {
+  protected changes: C[] = [];
 
   public clearChanges() {
     this.changes = [];
   }
 
-  protected addChange(change: Change) {
+  protected addChange(change: C) {
     this.changes.push(change);
   }
 }
