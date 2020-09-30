c9abe4d13159f704f56aa0ac3984b78f68bea978
Atobaum <kuuften@gmail.com>
2020-10-01 05:33:53 +0900

    feat: Aggregate root tracks changes

--- server/src/domain/shared/AggregateRoot.ts
+++ server/src/domain/shared/AggregateRoot.ts
@@ -1,3 +1,18 @@
 import Entity from "./Entity";
 
-export default abstract class AggregateRoot<K = number> extends Entity<K> {}
+type Change = {
+  type: string;
+  payload: any;
+};
+
+export default abstract class AggregateRoot<K = number> extends Entity<K> {
+  protected changes: Change[] = [];
+
+  public clearChanges() {
+    this.changes = [];
+  }
+
+  protected addChange(change: Change) {
+    this.changes.push(change);
+  }
+}
