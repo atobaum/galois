119204e821c09955751b406f7d462844c64b6ed2
Atobaum <kuuften@gmail.com>
2020-10-03 16:51:19 +0900

    fix: Set default client, server hosts

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -11,8 +11,8 @@ dotenv.config({
 });
 
 export default {
-  clientHost: env.CLIENT_HOST,
-  serverHost: env.SERVER_HOST,
+  clientHost: env.CLIENT_HOST || "",
+  serverHost: env.SERVER_HOST || "",
   port: parseInt(env.PORT!) || 4000,
   mongodbURI: env.MONGODB_URI,
   postgresql: {
