baa9f445ea84f0505350c9d08c478d0b3de2482d
Atobaum <kuuften@gmail.com>
2020-10-04 16:43:15 +0900

    feat: Add UPDATE_ZETTEL action

--- client/src/reducers/zettelReducer.ts
+++ client/src/reducers/zettelReducer.ts
@@ -2,6 +2,7 @@ import { Zettel } from "../models/Zettel";
 
 const CREATE_ZETTEL = "zettel/CREATE_ZETTEL" as const;
 const DELETE_ZETTEL = "zettel/DELETE_ZETTEL" as const;
+const UPDATE_ZETTEL = "zettel/UPDATE_ZETTEL" as const;
 
 export const addZetel = (zettel: Zettel) => ({
   type: CREATE_ZETTEL,
@@ -13,9 +14,15 @@ export const deleteZettelAction = (id: number) => ({
   payload: id,
 });
 
+export const updateZettelAction = (zettel: Zettel) => ({
+  type: UPDATE_ZETTEL,
+  payload: zettel,
+});
+
 type InboxAction =
   | ReturnType<typeof addZetel>
-  | ReturnType<typeof deleteZettelAction>;
+  | ReturnType<typeof deleteZettelAction>
+  | ReturnType<typeof updateZettelAction>;
 
 type InboxState = {
   zettels: Zettel[];
@@ -34,6 +41,12 @@ export default function inboxReducer(
       return { zettels: [...state.zettels, action.payload] };
     case DELETE_ZETTEL:
       return { zettels: state.zettels.filter((z) => z.id !== action.payload) };
+    case UPDATE_ZETTEL:
+      return {
+        zettels: state.zettels.map((z) =>
+          z.id === action.payload.id ? action.payload : z
+        ),
+      };
 
     default:
       return state;
