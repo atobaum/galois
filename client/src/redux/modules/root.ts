5d571b1a0363e335ddfc83e70960c80e89a06cc3
Atobaum <kuuften@gmail.com>
2020-10-05 03:45:32 +0900

    feat: Apply toast

--- client/src/redux/modules/root.ts
+++ client/src/redux/modules/root.ts
@@ -4,12 +4,14 @@ import coreReducer from "./coreReducer";
 import modalReducer from "./modalReducer";
 import zettelReducer from "./zettelReducer";
 import editorReducer from "./editorReducer";
+import { toastEpic, toastReducer } from "./toast";
 
-export const rootEpic = combineEpics();
+export const rootEpic = combineEpics(toastEpic);
 
 export const rootReducer = combineReducers({
   core: coreReducer,
   modal: modalReducer,
   zettel: zettelReducer,
   editor: editorReducer,
+  toast: toastReducer,
 });
