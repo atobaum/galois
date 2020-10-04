07094174e2f971e662b37103232d773e9d291b18
Atobaum <kuuften@gmail.com>
2020-10-05 00:06:27 +0900

    refactor: JWT_SECRET 제거 in token.ts

--- server/src/lib/token.ts
+++ server/src/lib/token.ts
@@ -1,15 +1,13 @@
 import jwt, { SignOptions } from "jsonwebtoken";
 import config from "../config";
 
-const JWT_SECRET = config.jwt.secret;
-
 export function generateToken<T>(
   payload: T,
   options: SignOptions = {}
 ): Promise<string> {
-  if (!JWT_SECRET) throw new Error("Missing JWT_SECRET");
+  if (!config.jwt.secret) throw new Error("Missing JWT_SECRET");
   return new Promise((resolve, reject) => {
-    jwt.sign(payload as any, JWT_SECRET!, options, (err, encoded) => {
+    jwt.sign(payload as any, config.jwt.secret!, options, (err, encoded) => {
       if (err) reject(err);
       else resolve(encoded);
     });
@@ -17,9 +15,9 @@ export function generateToken<T>(
 }
 
 export function decodeToken<T>(token: string): Promise<T> {
-  if (!JWT_SECRET) throw new Error("Missing JWT_SECRET");
+  if (!config.jwt.secret) throw new Error("Missing JWT_SECRET");
   return new Promise((resolve, rejecct) => {
-    jwt.verify(token, JWT_SECRET!, (err: any, decoded: any) => {
+    jwt.verify(token, config.jwt.secret!, (err: any, decoded: any) => {
       if (err) rejecct(err);
       else resolve(decoded as T);
     });
