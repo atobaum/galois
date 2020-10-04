9082a18e8a394f4a058665ac3ed7086ad7a66971
Atobaum <kuuften@gmail.com>
2020-10-05 00:03:28 +0900

    refactor: Remove Koa related codes in token.ts

--- server/src/lib/token.ts
+++ server/src/lib/token.ts
@@ -1,12 +1,11 @@
 import jwt, { SignOptions } from "jsonwebtoken";
-import { Context } from "koa";
 import config from "../config";
 
 const JWT_SECRET = config.jwt.secret;
 
 export function generateToken<T>(
   payload: T,
-  options: SignOptions
+  options: SignOptions = {}
 ): Promise<string> {
   if (!JWT_SECRET) throw new Error("Missing JWT_SECRET");
   return new Promise((resolve, reject) => {
@@ -26,41 +25,3 @@ export function decodeToken<T>(token: string): Promise<T> {
     });
   });
 }
-
-export function setTokens(
-  ctx: Context,
-  tokens: { accessToken: string; refreshToken?: string }
-): void {
-  // TODO: fix hard coded max age
-  ctx.cookies.set("access_token", tokens.accessToken, {
-    httpOnly: true,
-    maxAge: 60 * 60 * 1000, //1h
-  });
-
-  if (tokens.refreshToken) {
-    ctx.cookies.set("refresh_token", tokens.refreshToken, {
-      httpOnly: true,
-      maxAge: 7 * 24 * 60 * 60 * 1000, //7d
-    });
-  }
-}
-
-export function clearTokens(ctx: Context) {
-  ctx.cookies.set("access_token", "");
-  ctx.cookies.set("refresh_token", "");
-}
-
-type TokenData = {
-  exp: number;
-  iat: number;
-  sub?: string;
-};
-
-export type AccessTokenData = {
-  id: number;
-} & TokenData;
-
-export type RefreshTokenData = {
-  token_id: number;
-  email: String;
-} & TokenData;
