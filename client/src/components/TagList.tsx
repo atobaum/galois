55f74b85bc80fe1ba1a9db6364184d16410f8743
Atobaum <kuuften@gmail.com>
2020-10-06 23:37:27 +0900

    feat: Create SmallEditor and TagList

---
+++ client/src/components/TagList.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+/** @jsx jsx */
+import { jsx, css } from "@emotion/core";
+import Tag from "./Tag";
+
+const TagListCss = css`
+  display: flex;
+  span + span {
+    margin-left: 10px;
+  }
+`;
+
+const TagList: React.FC<{
+  tags: string[];
+  onTagClick?: (name: string) => void;
+}> = ({ tags, onTagClick }) => {
+  return (
+    <div css={TagListCss}>
+      {tags.map((tag) => (
+        <Tag
+          key={tag}
+          onClick={onTagClick ? () => onTagClick(tag) : undefined}
+          name={tag}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default TagList;
