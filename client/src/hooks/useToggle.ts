19ea0d08580a37f2fc33ed4074c62784bd29ad6a
Atobaum <kuuften@gmail.com>
2020-10-08 18:06:26 +0900

    chore: Change useToggle test to ts

--- client/src/hooks/useToggle.ts
+++ client/src/hooks/useToggle.ts
@@ -1,6 +1,6 @@
 import { useState, useCallback } from "react";
 
-export default function useToggle(initialValue = false) {
+export default function useToggle(initialValue = false): [boolean, () => void] {
   const [state, setState] = useState(initialValue);
   const toggle = useCallback(() => setState(!state), [state]);
 
