42d0ba80c6063a416f828e2b2ce14ec93083da42
Atobaum <kuuften@gmail.com>
2020-10-09 18:39:48 +0900

    feat: Set page title

--- client/src/pages/MainPage.tsx
+++ client/src/pages/MainPage.tsx
@@ -4,8 +4,10 @@ import StatusBar from "../components/core/StatusBar";
 import ZettelGrid from "../components/zettel-grid/ZettelGrid";
 import SmallEditor from "../components/main-page/SmallEditor";
 import { Container } from "@material-ui/core";
+import useSetTitle from "../hooks/useSetTitle";
 
 function MainPage() {
+  useSetTitle("Home");
   return (
     <div>
       <StatusBar />
