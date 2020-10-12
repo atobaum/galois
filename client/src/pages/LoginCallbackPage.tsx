bb9cb69c19f55db9905820f6328d6cd4bbdd2e4e
Atobaum <kuuften@gmail.com>
2020-10-12 18:00:11 +0900

    feat: Show error in toast if login failed 로그인 #35

--- client/src/pages/LoginCallbackPage.tsx
+++ client/src/pages/LoginCallbackPage.tsx
@@ -1,6 +1,11 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
+import { showToast } from "../redux/modules/toast";
+import { useDispatch } from "react-redux";
 
 export default function LoginCallbackPage() {
+  const dispatch = useDispatch();
+  const history = useHistory();
   const decodeQueryString = (q: string) =>
     q
       ? q
@@ -16,9 +21,12 @@ export default function LoginCallbackPage() {
   const params = decodeQueryString(hash);
   if (params.access_token) {
     window.localStorage.setItem("access_token", params["access_token"]);
+    //TODO 그 전의 url로 가기
+    window.location.href = params.next ? params.next : "/";
+  } else {
+    dispatch(showToast("로그인 실패"));
+    history.push("/");
   }
-  //TODO 그 전의 url로 가기
-  window.location.href = params.next ? params.next : "/";
 
   return <></>;
 }
