1d3252b751834709b236b7cb084659a3b6211e8a
atobaum <atobaum@gmail.com>
2020-10-28 23:47:21 +0900

    feat: Create login page

---
+++ client/src/pages/LoginPage.tsx
@@ -0,0 +1,38 @@
+import React, { useDebugValue } from "react";
+/** @jsx jsx */
+import { jsx, css } from "@emotion/core";
+import { showToast } from "../redux/modules/toast";
+import { useDispatch } from "react-redux";
+
+const LoginPageCss = css`
+  display: grid;
+  place-content: center;
+  height: 100vh;
+  grid-gap: 1rem;
+
+  button {
+    width: 5rem;
+    height: 3rem;
+    background-color: #95a89d;
+  }
+`;
+
+const LoginPage: React.FC = () => {
+  const dispatch = useDispatch();
+  return (
+    <div css={LoginPageCss}>
+      <a
+        href={
+          (process.env.REACT_APP_API_URL || "") + "/api/auth/google/redirect"
+        }
+      >
+        <button>로그인</button>
+      </a>
+      <button onClick={() => dispatch(showToast("아직 지원하지 않습니다."))}>
+        회원가입
+      </button>
+    </div>
+  );
+};
+
+export default LoginPage;
