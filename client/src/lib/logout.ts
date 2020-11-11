ec6ab6edf963bf47156c1543762e18453cd438a9
atobaum <atobaum@gmail.com>
2020-11-12 00:35:30 +0900

    fix: :bug: Fix type error

--- client/src/lib/logout.ts
+++ client/src/lib/logout.ts
@@ -4,5 +4,5 @@ export default function logout() {
   localStorage.removeItem("refresh_token");
   localStorage.removeItem("user");
 
-  if (loggedIn) location.reload();
+  if (loggedIn) window.location.reload();
 }
