b9843dbeee7b8b77332a2681f02c5c4375bd108c
atobaum <atobaum@gmail.com>
2020-11-11 23:59:35 +0900

    test: Write test code for logout function

--- client/src/lib/logout.ts
+++ client/src/lib/logout.ts
@@ -1,8 +1,8 @@
 export default function logout() {
-  const loggedIn = localStorage.getItem("refresh_token");
+  const loggedIn = localStorage.getItem("access_token");
   localStorage.removeItem("access_token");
   localStorage.removeItem("refresh_token");
   localStorage.removeItem("user");
 
-  if (loggedIn) window.location.reload();
+  if (loggedIn) location.reload();
 }
