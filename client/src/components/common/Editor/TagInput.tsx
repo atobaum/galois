19af3e7a1bdf32ecb998be628c62a7c3ebaf7994
atobaum <atobaum@gmail.com>
2020-11-16 18:00:28 +0900

    feat: Create TagInput which uses react-hook-form

--- client/src/components/common/Editor/TagInput.tsx
+++ client/src/components/common/Editor/TagInput.tsx
@@ -1,39 +1,40 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 /** @jsx jsx */
 import { jsx, css } from "@emotion/core";
-import TagList from "./TagList";
 import { Input } from "@material-ui/core";
+import { EditorCompoenentInput } from "./@types";
+import TagList from "../TagList";
 
-const TagInputCss = css`
-  display: flex;
-  .tag-input {
-    margin-left: 1rem;
-  }
+const TagInputCss = css``;
 
-  & * {
-    font-size: 1rem;
-  }
-`;
-
-// deprecated
-const TagInput: React.FC<{
-  tags?: string[];
-  onChange: (tags: string[]) => void;
-}> = ({ tags = [], onChange }) => {
+const TagInput: EditorCompoenentInput<
+  "register" | "setValue" | "watch",
+  { defaultValue?: string[] }
+> = ({ register, setValue, watch, defaultValue = [] }) => {
   const [input, setInput] = useState("");
+  const tags: string[] = watch("tags", [] as string[]);
   const handleTagOnKeypress: React.KeyboardEventHandler<HTMLInputElement> = (
     evt
   ) => {
     if (evt.key === "Enter" || evt.which === 13) {
       evt.preventDefault();
       const tag = input.trim();
-      if (tag.length > 0 && !tags.includes(tag)) onChange([...tags, tag]);
+      if (tag.length > 0 && !tags.includes(tag))
+        setValue("tags", [...tags, tag]);
       setInput("");
     }
   };
 
+  useEffect(() => {
+    register("tags");
+    setValue("tags", defaultValue);
+  }, []);
+
   const onTagClick = (tag: string) => {
-    onChange(tags.filter((t) => t !== tag));
+    setValue(
+      "tags",
+      tags.filter((t) => t !== tag)
+    );
   };
 
   return (
