adebbdd290a2bce0d9bb79aa1a671861586f9b04
atobaum <atobaum@gmail.com>
2020-11-17 23:24:59 +0900

    docs: 스토리북 정리

--- client/src/stories/SmallEditor.stories.tsx
+++ client/src/stories/SmallEditor.stories.tsx
@@ -4,16 +4,40 @@ import { action } from "@storybook/addon-actions";
 import { Meta } from "@storybook/react/types-6-0";
 import React from "react";
 import SmallEditor from "../components/common/editor/SmallEditor";
+import ZettelType from "../types/zettel-type";
+import SourceType from "../types/source-type";
+import ContentType from "../types/content-type";
 
 export default {
   title: "components|editor/SmallEditor",
   component: SmallEditor,
 } as Meta;
 
-export const standard = (args: any) => {
+const Template: Story<{ defaultZettel: Zettel }> = (args: any) => {
   return <SmallEditor {...args} />;
 };
 
-standard.args = {
-  onSubmit: action("onSubmit"),
+export const NewZettel = Template.bind({});
+
+const bookmarkZettle: Zettel = {
+  id: "zettle-uuid-id",
+  number: 201111001,
+  title: "bookmark title",
+  content: "bookmark content",
+  type: ZettelType.BOOKMARK,
+  tags: ["tag1", "tag2"],
+  createdAt: new Date("2020-11-11"),
+  updatedAt: new Date("2020-11-11"),
+  meta: {
+    source: {
+      type: SourceType.URL,
+      data: "https://www.example.com",
+    },
+    renderer: ContentType.MARKDOWN,
+  },
+} as Zettel;
+export const Bookmark = Template.bind({});
+
+Bookmark.args = {
+  defaultZettel: bookmarkZettle,
 };
